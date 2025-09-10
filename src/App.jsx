e6b32bef9c83f7c4a212b67f5dbb6d72b362babe
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-10 15:56:51 +0430

    Files Structured

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Cart from "./components/Cart/Cart";
+import Products from "./components/Shop/Products";
+import Layout from "./components/Layout/Layout";
 
 
 function App() {
     return (
-        <>
+        <Layout>
             <Cart />
-            
-        </>
+            <Products />
+        </Layout>
     );
 };
 
