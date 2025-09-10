f236d3cfbf50900e31e32b1432d2401437d201a7
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-10 15:17:26 +0430

    First Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import StateLogin from "./components/StateLogin";
 
 
 function App() {
     return (
         <>
-            <Header />
             <main>
-                <StateLogin />
             </main>
         </>
     );
