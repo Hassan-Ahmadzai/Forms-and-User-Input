6e53cf6710a1062d0122eb80d9c9a36286276e79
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-10 15:38:01 +0430

    Functions created

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import Card from "../UI/Card"
+
+
+const Cart = (props) => {
+    return (
+        <Card>
+            <h2>Your Shopping Cart</h2>
+            <ul>
+
+            </ul>
+        </Card>
+    );
+};
+
+export default Card;
---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,34 @@
+import classes from './CartItem.module.css';
+
+
+const CartItem = (props) => {
+    const { title, quantity, total, price } = props.item;
+
+    return (
+        <li className={classes.item}>
+            <header>
+                <h3>{title}</h3>
+
+                <div className={classes.price}>
+                    ${total.toFixed(2)}{' '}
+                    <span className={classes.itemprice}>
+                        (${price.toFixed(2)}/item)
+                    </span>
+                </div>
+            </header>
+
+            <div className={classes.details}>
+                <div className={classes.quantity}>
+                    X <span>{quantity}</span>
+                </div>
+
+                <div className={classes.action}>
+                    <button>-</button>
+                    <button>+</button>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default CartItem;
