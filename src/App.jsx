395e402f624bbcaaab20a38a200cbe3829750624
Shakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-10 16:01:11 -1200

    All Folders Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import Cart from "./components/Cart/Cart";
-import Products from "./components/Shop/Products";
-import Layout from "./components/Layout/Layout";
-
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-    return (
-        <Layout>
-            <Cart />
-            <Products />
-        </Layout>
-    );
-};
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
 
 export default App;
