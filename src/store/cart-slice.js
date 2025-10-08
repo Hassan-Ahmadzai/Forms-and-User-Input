d2babe7035afce726ab6fc23be5ed23d4d50e7c8
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-10-08 09:49:05 +0430

    Functions Updated

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -12,7 +12,7 @@ createSlice({
             const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
 
-            if (!existingItem) {
+            if (!existingItem) {    
                 state.items.push({
                     itemId: newItem.id,
                     price: newItem.price,
@@ -25,6 +25,15 @@ createSlice({
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             };
         },
-        removeItemFromCart() {},
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter((item) => item.id !== id);
+            } else {
+                existingItem.quantity--;
+            }
+        },
     },
 });
